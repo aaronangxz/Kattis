0c9cb7dde17020bc1caef9c8b2229b31fc108d07
aaronangxz <angxze@gmail.com>
2020-10-11 03:43:24 +0800

    Updated Fizz Buzz

---
+++ FizzBuzz.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int x, y, n;
+    scanf("%d %d %d",&x,&y,&n);
+    for (int i = 1; i <= n ; i++)
+    {
+        if (i%x==0 && i%y==0)
+        {
+            cout << "FizzBuzz" << endl;
+        }
+        else if (i%x==0)
+        {
+            cout << "Fizz" << endl;
+        }
+        
+        else if (i%y==0)
+        {
+            cout << "Buzz" << endl;
+        }
+        else
+        {
+            cout << i << endl;
+        }
+    }
+}
