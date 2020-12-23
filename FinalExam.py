b439e3e88fe1a3dd2a190cbb1c337acd077705de
aaronangxz <angxze@gmail.com>
2020-12-24 01:26:24 +0800

    Updated Final Exam.py

--- FinalExam.py
+++ FinalExam.py
@@ -1,16 +1,25 @@
+# Create empty lists, initialize count
 correct = []
 hanh = []
 count = 0
+# Input number of questions
 n = int(input())
+# Loop n times to input correct answers
 for i in range(0,n):
     ans = input()
+    # Add correct answers to list
     correct.append(ans)
+    # Reset list
     ans = ''
     
+# Duplicate correct answers to hanh's answers
 hanh = correct
 
+# Loop n - 1 times since hanh's answer is always one more than correct answer
 for i in range(0,n - 1):
+    # If the current answer matches hanh's next answer
     if correct[i] == hanh[i + 1]:
+        # He got it correct
         count += 1
 
 print(count)
