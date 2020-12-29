b094057e1586784f7a37f8d7725db5b89432b444
aaronangxz <angxze@gmail.com>
2020-12-29 16:51:39 +0800

    Updated DrunkVigenere

---
+++ DrunkVigenere.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    string msg, key, moved;
+    vector <string> newstr;
+    cin >> msg;
+    cin >> key;
+
+    for (int i = 0; i < key.size(); i++)
+    {
+        moved = msg[i] + (key[i] - 65);
+    
+        newstr.push_back(moved);
+    }
+    
+    for (int j = 0; j < newstr.size(); j++)
+    {
+        cout << newstr[j];
+    }
+    
+}
